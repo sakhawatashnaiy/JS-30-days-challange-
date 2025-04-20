74bd26003cfd4f8033bad9b158b1f5c5b2b5be43
sakhawatashnaiy <sakhawatashnaiy09@gmail.com>
2025-04-20 10:18:29 +0500

    updated class-1

--- class-1/app.js
+++ class-1/app.js
@@ -1,18 +1,24 @@
-// function removeTransition(e) {
-//     if (e.propertyName !== 'transform') return;
-//     e.target.classList.remove('playing');
-//   }
-
-//   function playSound(e) {
-//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-//     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-//     if (!audio) return;
-
-//     key.classList.add('playing');
-//     audio.currentTime = 0;
-//     audio.play();
-//   }
-
-//   const keys = Array.from(document.querySelectorAll('.key'));
-//   keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-//   window.addEventListener('keydown', playSound);
+    
+    const playstae = (e) =>{
+        if(e.propertyName=== 'transform') return 
+        e.target .classList.remove('.playing')
+    }
+      
+    const playMusic = (e) =>{
+        const sound = document.querySelector(`audio[data-key='${e.keyCode}']`);
+        // console.log(sound);
+        const key = document.querySelector(`.key[data-key='e.keyCode']`);
+        // console.log(key)
+          
+         if(!sound) return ;
+          key.classList.add('.playing');
+          sound.currentTime =  0 ;
+          sound.play();
+    }
+       const keys = Array.from(document.querySelectorAll('.key'));
+       keys.forEach(sound);
+       function sound(e){
+           e.addEventListener('transitioned',playstae);
+       }
+      window.addEventListener('keydown' , playMusic);
+  
